83a5c91cbb688b391ae2e6ed519506a16622bc78
Cameron Dix <cdix95@gmail.com>
2018-10-22 00:07:49 -0700

    initial commit

---
+++ dist/config/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.default = {
+	"port": 3005,
+	"mongoUrl": "mongodb://localhost:27017/Restaurant_API"
+};
+//# sourceMappingURL=index.js.map
